863172026bc689d0ba63b024494b3f94d401f8b2
@betty2926 <betelhem.getachew2926@gmail.com>
2023-07-14 11:20:45 -0700

    router added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,23 @@
 import { MantineProvider } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import "./App.css";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
-    const form = useForm({
-      initialValues: {
-        name: "",
-        email: "",
-      },
-    });
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+    },
+  });
   return (
     <MantineProvider>
-      <div>
-        Hello World!
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   );
 }
