211e2d9de88665ff42ace7b30d743d3adbd6accf
@betty2926 <betelhem.getachew2926@gmail.com>
2023-07-15 14:44:23 -0700

    typeScript form compelete

---
+++ frontend/src/pages/addForm.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import axios from "axios";
+import { Button } from "@mantine/core";
+
+interface treeForm {
+  map(arg0: (tree: any) => JSX.Element): import("react").ReactNode;
+  name: string;
+  description: string;
+  _id: string;
+  names: string;
+}
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  description: yup.string().required(),
+  names: yup.string(),
+});
+function AddForm() {
+  const [trees, setTrees] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<treeForm>({
+    //  resolver: yupResolver<yup.AnyObject>(schema),
+  });
+
+  const getTrees = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/treeStructure/allTrees/"
+      );
+      setTrees(response.data);
+      //   console.log(response.data);
+      return;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTrees();
+  }, []);
+  const submitForm = (data: treeForm) => console.log(data);
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit(submitForm)}
+        className="bg-white w-[500px] mx-10 my-10 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <label className="pr-3">name</label>
+        <input
+          type="string"
+          // name="name"
+          className="shadow appearance-none border rounded w-[180px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          {...register("name", { required: true, minLength: 3, maxLength: 20 })}
+        />
+        <p className="block text-red-600 font-[13px]">{errors.name?.message}</p>
+
+        <div className="block my-5">
+          <label className="pr-3">description</label>
+          <input
+            type="string"
+            // name="description"
+            className="shadow appearance-none border rounded w-[180px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            {...register("description")}
+          />
+          {/* {errors.description?.message} */}
+        </div>
+
+        <div className="block mb-3">
+          <label className="pr-3">select parent</label>
+          <select id="names" {...register("names")}>
+            <option></option>
+            {trees &&
+              trees.map((tree: any) => (
+                <>
+                  <option value={tree._id}>{tree.name}</option>
+                </>
+              ))}
+          </select>
+        </div>
+        <Button
+          type="submit"
+          className="bg-black hover:bg-indigo-400 ml-5"
+          radius="md"
+          size="md"
+        >
+          submit
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default AddForm;
