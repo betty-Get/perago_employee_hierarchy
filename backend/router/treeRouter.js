666e3e1d1207d79046b4f6895743e191b9f7ede0
@betty2926 <betelhem.getachew2926@gmail.com>
2023-07-16 21:43:38 -0700

    update and delete role updated

--- backend/router/treeRouter.js
+++ backend/router/treeRouter.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/addTrees", async (req, res) => {
   try {
     const { name, description, parentId } = req.body;
+
     const parent = await treeSchema.create({ name, description, parentId });
     res.status(200).json(parent);
   } catch (error) {
@@ -28,12 +29,12 @@ router.get("/allTrees", async (req, res) => {
 });
 
 //update trees
-router.patch("/updateTree/:id", async (req, res) => {
-  const { id } = req.params;
+router.patch("/updateEmployeeRole/:roleId", async (req, res) => {
+  const { roleId } = req.params;
   const { name, description } = req.body;
   try {
     const updated = await treeSchema.findByIdAndUpdate(
-      { _id: id },
+      { _id: roleId },
       { name, description },
       { new: true }
     );
@@ -45,10 +46,10 @@ router.patch("/updateTree/:id", async (req, res) => {
 });
 
 //remove trees
-router.delete("/deleteTree/:id", async (req, res) => {
-  const { id } = req.params;
+router.delete("/deleteEmployeeRole/:roleId", async (req, res) => {
+  const { roleId } = req.params;
   try {
-    const deleted = await treeSchema.findByIdAndRemove({ _id: id });
+    const deleted = await treeSchema.findByIdAndRemove({ _id: roleId });
     res.status(200).json({ mssg: "deleted successfully" });
   } catch (error) {
     res.status(500).json(error);
