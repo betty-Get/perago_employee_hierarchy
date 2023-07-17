e51741f33e608bbe718903898d7a5cc49087bc9d
@betty2926 <betelhem.getachew2926@gmail.com>
2023-07-17 01:30:50 -0700

    navbar customized

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mantine/core";
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <div>
-      <div className="h-[150px] w-full bg-indigo-600 p-8">
-        <div className="w-[900px] h-[60px] bg-white block mx-auto rounded-full">
-          <div>
-            <p className="pl-5">Home</p>
-          </div>
+      <div className="h-[150px] w-full p-8 flex space-x-96">
+        <div>
+          <Button
+            type="submit"
+            className="bg-slate-500 hover:bg-slate-300 hover:text-lime-600 ml-5"
+            radius="md"
+            size="lg"
+            onClick={() => navigate("/")}
+          >
+            Home
+          </Button>
+        </div>
+        <div className="">
+          <img src="/perago.jpg" alt="image" className="h-[80px] w-[200px]" />
         </div>
       </div>
     </div>
